58aaac70b5bd7b63ff076c1dbe14a753210f3dca
kzk0829 <kzk0829kop@gmail.com>
2020-07-01 00:36:35 +0900

    add p03

---
+++ src/main/scala/01-10/P03.scala
@@ -0,0 +1,14 @@
+object p03 {
+    def main(args: Array[String]): Unit = {
+        def nth[A](n: Int, list: List[A]): A = {
+            list match {
+                case x :: _ if n == 0 => x
+                case _ :: xs if n > 0 => nth(n-1, xs)
+                case _ => throw new IllegalArgumentException("Invalid val")
+            }
+        }
+        println(nth(3, List(1,2,3,4,5)))
+        println(nth(4, List("a", "b", "c", "d", "e", "f")))
+        println(nth(5, List(1,2)))
+    }
+}
